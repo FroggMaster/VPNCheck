95179c72abbf7fd24daeda06380ea1b5a3afbd18
Dom <FroggMaster@users.noreply.github.com>
2023-03-06 00:33:16 -0800

    Update vpncheck.sh
    - Added command line option -c to pass config file
    - Added config.ini that is stored in the same location as the script. This will store the configuration file location, meaning -c only needs to be passed once. 
    - If no configuration file is passed one will be requested VIA a prompt to the user, the provided config will be stored in the config.ini

--- vpncheck.sh
+++ vpncheck.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Define the name of the OpenVPN interface and the connection script
+# Define the name of the OpenVPN interface
 INTERFACE="tun0"
-CONNECTSCRIPT="/path/to/connection/script.sh"
 
 # Define the log file and maximum log size in MB
 LOGFILE="./vpncheck.log"
@@ -17,21 +16,52 @@ RED='\033[0;31m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
 
-# Check if another instance of this script is already running
-PIDFILE=./vpncheck.pid
-if [ -f "$PIDFILE" ] && ps -p $(cat $PIDFILE) > /dev/null; then
-    echo "Another instance of this script is already running, exiting..."
-    exit 1
-else
-    echo $$ > $PIDFILE
+# Define config file path
+CONFIG_FILE="./config.ini"
+
+# Get the connection script path from command line argument if provided
+while getopts "c:" opt; do
+    case ${opt} in
+        c )
+            CONNECTSCRIPT="$OPTARG"
+            printf 'CONNECTSCRIPT="%s"\n' "$OPTARG" >> "$CONFIG_FILE"
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" 1>&2
+            exit 1
+            ;;
+    esac
+done
+
+# If no connection script is provided via command line argument, check the config file
+if [ -z "$CONNECTSCRIPT" ]; then
+    # Check if config file exists and read the connection script path from it
+    if [ -f "$CONFIG_FILE" ]; then
+        . "$CONFIG_FILE"
+    fi
 fi
 
-# Function to remove the PID file on exit
-cleanup() {
-    rm -f $PIDFILE
-    exit
-}
-trap cleanup EXIT
+# Prompt user for connection script path if not defined in config file or command line argument
+if [ -z "$CONNECTSCRIPT" ]; then
+    read -p "Please enter the path to the OpenVPN connection script: " CONNECTSCRIPT
+    printf '%s\n' "CONNECTSCRIPT=$CONNECTSCRIPT" >> "$CONFIG_FILE"
+fi
+
+# Check if the OpenVPN connection script is executable
+if ! [ -x "$CONNECTSCRIPT" ]; then
+    echo "The OpenVPN connection script at $CONNECTSCRIPT is not executable, please make it executable and try again."
+    exit 1
+fi
+
+# Check if another instance of this script is already running
+if [ -f "$PIDFILE" ]; then
+    echo "Another instance of this script is already running, exiting..."
+    exit 1
+fi
+
+# Save the current process ID to a file
+PIDFILE="./vpncheck.pid"
+echo "$$" > "$PIDFILE"
 
 # Check if OpenVPN is running on the specified interface
 if ifconfig $INTERFACE &> /dev/null ; then
@@ -54,3 +84,6 @@ if [ $LOGSIZE -gt $MAXLOGSIZE ]; then
     echo "Resetting log file..."
     echo "" > $LOGFILE
 fi
+
+# Remove PID file
+rm "$PIDFILE"
